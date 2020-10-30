05fe612b6c0f7d13bb768ed5ed2b614be22dc6d8
Antonio López <alopez@tacitknowledge.com>
2020-10-30 00:30:49 -0600

    Creating git repo

---
+++ NetKatas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetKatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            Console.WriteLine(TCP.TraverseStates(new[] { "APP_ACTIVE_OPEN", "RCV_SYN_ACK", "RCV_FIN" }));
+        }
+    }
+}
